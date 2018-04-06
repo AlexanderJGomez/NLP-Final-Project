e2c7b0eb17ff8de9e767e952c35327dad22a2992
Matthew Piorko <Piorko.m@husky.neu.edu>
2018-04-06 05:53:59 -0400

    Adjusted variables in modified pageRank

--- textrank.py
+++ textrank.py
@@ -3,7 +3,7 @@ from nltk.cluster.util import cosine_distance
 
 stopwords = set(open('stopwords.txt', 'r').read().split('\n'))
 
-def similarity(sentence1, sentence2, lambdaScaling=.001):
+def similarity(sentence1, sentence2, sameSegment, lambdaScaling=.001):
     words = {}
     index = 0
 
@@ -28,7 +28,10 @@ def similarity(sentence1, sentence2, lambdaScaling=.001):
         if word not in stopwords:
             vector2[words[word]] += 1
 
-    return max(1 - cosine_distance(vector1, vector2), lambdaScaling)
+    distance = 1 - cosine_distance(vector1, vector2)
+    if sameSegment:
+        distance *= 1.1
+    return min(1, max(distance, lambdaScaling))
 
 def pagerank(A, eps=0.0001, d=0.85):
     P = np.ones(len(A)) / len(A)
@@ -39,18 +42,30 @@ def pagerank(A, eps=0.0001, d=0.85):
             return new_P
         P = new_P
 
-def createSummaryMatrix(sentences):
+def modifiedPagerank(A, eps=0.0001, d=0.85, alpha=0.1):
+    P = np.ones(len(A)) / len(A)
+    while True:
+        new_P = np.ones(len(A)) * (1 - d) / len(A) + d * A.T.dot(P)
+        delta = abs((new_P - P).sum())
+        if delta <= eps:
+            return new_P
+        P = new_P
+
+def createSummaryMatrix(sentences, sameSegments=None):
     matrix = np.zeros((len(sentences), len(sentences)))
 
     for i, sentence1 in enumerate(sentences):
         for j, sentence2 in enumerate(sentences):
-            matrix[i][j] = similarity(sentence1, sentence2)
+            if sameSegments is not None:
+                matrix[i][j] = similarity(sentence1, sentence2, True)
+            else:
+                matrix[i][j] = similarity(sentence1, sentence2, False)
 
     # normalize the matrix row-wise
     for idx in range(len(matrix)):
         matrix[idx] /= matrix[idx].sum()
 
-    return pagerank(matrix)
+    return modifiedPagerank(matrix)
 
 def createSummary(fileName, separator='\n', summaryLength=5):
     sentences = open(fileName, 'r', encoding='utf8').read().split(separator)
@@ -67,10 +82,14 @@ def baseline(fileName, separator='\n', segmentSeparator='\n###\n'):
     segments = open(fileName, 'r', encoding='utf8').read().split(segmentSeparator)
     segments = list(map(lambda segment: segment.split(separator), segments))
     sentences = []
+    sameSegments = {}
     for s in segments:
-        sentences.extend(s)
+        for sentence1 in s:
+            for sentence2 in s:
+                sameSegments[(sentence1, sentence2)] = true
+            sentences.append(sentence1)
 
-    result = createSummaryMatrix(sentences)
+    result = createSummaryMatrix(sentences, sameSegments)
 
     summaries = []
     idx = 0
